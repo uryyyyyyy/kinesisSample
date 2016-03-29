3180b1c6034b64c5c360d9573f6ec7d9b4fd6464
uryyyyyyy <koki305@gmail.com>
2016-03-30 00:38:20 +0900

    add some

--- src/main/scala/com/github/uryyyyyyy/kinesis/helloWorld/Hello.scala
+++ src/main/scala/com/github/uryyyyyyy/kinesis/helloWorld/Hello.scala
@@ -2,7 +2,8 @@ package com.github.uryyyyyyy.kinesis.helloWorld
 
 import java.nio.charset.Charset
 
-import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.{EnvironmentVariableCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.regions.{Regions, Region}
 import com.amazonaws.services.kinesis.AmazonKinesisClient
 import com.amazonaws.services.kinesis.model.DescribeStreamRequest
 
@@ -10,16 +11,19 @@ import scala.collection.JavaConversions._
 
 
 object Hello {
+
+	val streamName = "sampleStream"
+
 	def main(args: Array[String]): Unit = {
 
-		val accessKey = ""
-		val secretKey = ""
-		val kinesis = new AmazonKinesisClient(new BasicAWSCredentials(accessKey, secretKey))
+		val credential = new EnvironmentVariableCredentialsProvider()
+		val kinesis = new AmazonKinesisClient(credential)
+		kinesis.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1))
 		//デコーダー
 		val decoder = Charset.forName("UTF-8").newDecoder()
 		//シャード情報を取得
 		val describeStreamRequest = new DescribeStreamRequest()
-		describeStreamRequest.setStreamName("kinesis.name")
+		describeStreamRequest.setStreamName(streamName)
 		val describeStreamResult = kinesis.describeStream(describeStreamRequest)
 		val shards =  describeStreamResult.getStreamDescription().getShards()
 		shards.foreach(println)
